8d56fe59a5b47f58fe1d7bf0d7d13654acaa44c2
Maciek Zemlo <38382353+yabool2001@users.noreply.github.com>
2023-12-30 01:47:08 +0100

    aaaaa

--- promise.js
+++ promise.js
@@ -1,8 +1,10 @@
 // Na podstawie JavaScript Promises In 10 Minutes (https://www.youtube.com/watch?v=DHvZLI7Db8E) by Web Dev Simplified
 
+let quest = {"q": "a"}
 
 // Scenariusz 1 z resolve:
-let quest = {"q": "a"}
+
+console.log ( "Scenariusz 1 z resolve." )
 
 let p = new Promise
 (
@@ -11,22 +13,19 @@ let p = new Promise
         // Tutaj implementuję działania, które obicałem zrobić.
         let result = null
         if ( 'q' in quest )
-        {
             // Jeśli udało mi się odczytać wartość, to używam resolve
             resolve ( `Wartość q = ${quest.q}` )
-        }
         else
-        {
             // Jeśli nie udało mi się odczytać wartość, to zwracam reject
             reject ( "No data" )
-        }
     }
 )
 
 p.then
 (
     ( r ) => console.log ( r )
-).catch
+)
+.catch
 (
     ( r ) => console.log ( r )
 )
@@ -34,7 +33,10 @@ p.then
 
 
 // Scenariusz 2 z reject:
-quest = {"w": "a"}
+
+console.log ( "Scenariusz 2 z reject: No data." )
+
+quest = {"w": "b"}
 
 function f ()
 {
@@ -43,15 +45,11 @@ function f ()
         ( resolve , reject ) =>
         {
             if ( 'q' in quest )
-            {
                 // Jeśli udało mi się odczytać wartość, to używam resolve
                 resolve ( `Wartość q = ${quest.q}` )
-            }
             else
-            {
                 // Jeśli nie udało mi się odczytać wartość, to zwracam reject
                 reject ( "No data" )
-            }
         }
     )
 }
@@ -59,7 +57,92 @@ function f ()
 f().then
 (
     ( r ) => console.log ( r )
-).catch
+)
+.catch
+(
+    ( r ) => console.log ( r )
+)
+
+
+// Scenariusz 3 z Promise.all:
+
+console.log ( "Scenariusz 3 z resolve i reject. I nie wiem czemu nie chce działać z resolve" )
+
+quest = { "q": "a" , "w": "b" }
+const data1 = new Promise
+(
+    ( resolve , reject ) => 
+    {
+        if ( 'q' in quest )
+            // Jeśli udało mi się odczytać wartość, to używam resolve
+            resolve ( `Wartość q = ${quest.q}` )
+        else
+            // Jeśli nie udało mi się odczytać wartość, to zwracam reject
+            reject ( "No data" )
+    }
+)
+const data2 = new Promise
+(
+    ( resolve , reject ) =>
+    {
+        if ( 'w' in quest )
+            // Jeśli udało mi się odczytać wartość, to używam resolve
+            resolve ( `Wartość w = ${quest.w}` )
+        else
+            // Jeśli nie udało mi się odczytać wartość, to zwracam reject
+            reject ( "No data" )
+    }
+)
+
+Promise.all
+(
+    [ data1 , data2 ]
+).then
+(
+    ( r ) => console.log ( r )
+)
+.catch
+(
+    ( r ) => console.log ( r )
+)
+
+
+
+// Scenariusz 4 z Promise.race:
+console.log ( "Scenariusz 4 z resolve." )
+const data3 = new Promise
+(
+    ( resolve , reject ) => 
+    {
+        if ( 'q' in quest )
+            // Jeśli udało mi się odczytać wartość, to używam resolve
+            resolve ( `Wartość q = ${quest.q}` )
+        else
+            // Jeśli nie udało mi się odczytać wartość, to zwracam reject
+            reject ( "No data" )
+    }
+)
+const data4 = new Promise
+(
+    ( resolve , reject ) =>
+    {
+        if ( 'w' in quest )
+            // Jeśli udało mi się odczytać wartość, to używam resolve
+            resolve ( `Wartość w = ${quest.w}` )
+        else
+            // Jeśli nie udało mi się odczytać wartość, to zwracam reject
+            reject ( "No data" )
+    }
+)
+
+Promise.race
+(
+    [ data1 , data2 ]
+).then
+(
+    ( r ) => console.log ( r )
+)
+.catch
 (
     ( r ) => console.log ( r )
 )
